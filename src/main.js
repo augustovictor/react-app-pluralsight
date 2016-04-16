3247150042f8250b68e1638b74b2f7d8bca07424
Victor <victoraweb@gmail.com>
2016-04-16 14:21:54 -0300

    Generating bundle.css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 $ = jQuery = require( 'jquery' );
-var React = require( 'react' );
-var Home = require( './components/homepage' );
+// var React = require( 'react' );
+// var Home = require( './components/homepage' );
 
-React.render(<Home />, document.getElementById('app'));
+var App = console.log( 'Hellow world from Browserify' );
+
+module.exports = App;
+
+// React.render(<Home />, document.getElementById('app'));
