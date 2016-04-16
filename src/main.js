da9a584b8adf9a49da0bc1dfc0336e0b5fcb88da
Victor <victoraweb@gmail.com>
2016-04-16 17:04:23 -0300

    Mock api

--- src/main.js
+++ src/main.js
@@ -1,9 +1,34 @@
+/** @jsx React.DOM */
 $ = jQuery = require( 'jquery' );
-// var React = require( 'react' );
-// var Home = require( './components/homepage' );
+var React = require( 'react' );
+var Home = require( './components/homepage' );
+var About = require('./components/about/aboutpage');
+var Header = require('./components/common/header');
 
-var App = console.log( 'Hellow world from Browserify' );
+var App = React.createClass({
+    render: function() {
+        var Child;
 
-module.exports = App;
+        switch (this.props.route) {
+            case 'about': Child = About; break;
+            default: Child = Home;
+        }
 
-// React.render(<Home />, document.getElementById('app'));
+        return (
+            <div>
+                <Header />
+                <Child />
+            </div>
+        )
+    }
+});
+
+function render() {
+    var route = window.location.hash.substr(1);
+    React.render(<App route={route} />, document.getElementById('app'));
+}
+
+window.addEventListener('hashchange', render);
+render();
+
+React.render(<Home />, document.getElementById('app'));
